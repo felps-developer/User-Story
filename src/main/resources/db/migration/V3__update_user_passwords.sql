6a8770bda298ddcaab8c61acb4506d7b20d0375c
Felipe25687 <felipemofernandes@gmail.com>
2025-11-25 01:13:12 -0300

    Update user password migration script to use BCrypt hash with strength 12 for improved security compliance

--- src/main/resources/db/migration/V3__update_user_passwords.sql
+++ src/main/resources/db/migration/V3__update_user_passwords.sql
@@ -1,6 +1,6 @@
 -- Atualiza senhas dos usuários
--- BCrypt hash para "senha123" com strength 15 (conforme requisito)
--- Hash: $2a$15$4umnMu8qBecMhoK4Q4SfouVygnmI39j5P0phuLL/ioMVBOMPHBwJC
+-- BCrypt hash para "senha123" com strength 12 (conforme requisito)
+-- Hash: $2a$12$4umnMu8qBecMhoK4Q4SfouVygnmI39j5P0phuLL/ioMVBOMPHBwJC
 
 UPDATE usuario SET senha = '$2a$12$4umnMu8qBecMhoK4Q4SfouVygnmI39j5P0phuLL/ioMVBOMPHBwJC' WHERE email = 'joao.silva@empresa.com';
 UPDATE usuario SET senha = '$2a$12$4umnMu8qBecMhoK4Q4SfouVygnmI39j5P0phuLL/ioMVBOMPHBwJC' WHERE email = 'maria.santos@empresa.com';
